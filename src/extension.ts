c5d4fea916d6b52f2191bd7f726e3cb024ebd27e
pattazl <pattazl@163.com>
2022-11-06 23:49:00 +0800

    add the buttons in search page

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {commands} from 'vscode';
 //import {vscAnalyze,initPara,vscClean,vscMove,vscDownload, vscUpload,vscInsertClip,
 //	vscConvertImageFormat,vscConvertImageLink}  from './index';
 // this method is called when your extension is activated
@@ -16,20 +17,51 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	//const config = vscode.workspace.getConfiguration(""); // searchFilter.inComments
 	// if(!initPara()){return;} // 从配置中获取初始化参数
-	let dispInComments = vscode.commands.registerCommand("searchFilter.inComments", async (textEditor: vscode.TextEditor) => {
+	let dispDoFilter = commands.registerCommand("searchFilter.doFilter", async (textEditor: vscode.TextEditor) => {
 		//if(!initPara()){return;} // 参数可能更新，重新从配置中获取初始化参数
 		//vscAnalyze();
+		let ctx = context;
 		//config.searchFilter.inComments = true
-		vscode.commands.executeCommand('setContext', 'searchFilter.inComments', false);
+		commands.executeCommand('setContext', 'searchFilter.doFilterFlag', true);
+		// 默认设置为代码中过滤
+		commands.executeCommand('setContext', 'searchFilter.inCommentsFlag', false);
+		// 默认全部代码
+		commands.executeCommand('setContext', 'searchFilter.StringFlag', 0);
 	})
-	let dispNotInComments = vscode.commands.registerCommand("searchFilter.notInComments", async () => {
+	let dispDoNotFilter = commands.registerCommand("searchFilter.doNotFilter", async (textEditor: vscode.TextEditor) => {
 		//if(!initPara()){return;} // 参数可能更新，重新从配置中获取初始化参数
 		//vscClean();
-		vscode.commands.executeCommand('setContext', 'searchFilter.inComments', true);
+		commands.executeCommand('setContext', 'searchFilter.doFilterFlag', false);
 	})
-	
+
+	let dispInComments = commands.registerCommand("searchFilter.inComments", async (textEditor: vscode.TextEditor) => {
+		commands.executeCommand('setContext', 'searchFilter.inCommentsFlag', true);
+	})
+	let dispNotInComments = commands.registerCommand("searchFilter.notInComments", async() => {
+		commands.executeCommand('setContext', 'searchFilter.inCommentsFlag', false);
+		// 默认代码的所有部分检查
+		commands.executeCommand('setContext', 'searchFilter.StringFlag', 0);
+	})
+
+	let dispInString = commands.registerCommand("searchFilter.inString", async () => {
+		commands.executeCommand('setContext', 'searchFilter.StringFlag', 1);
+	})
+	let dispNotInString = commands.registerCommand("searchFilter.notInString", async () => {
+		commands.executeCommand('setContext', 'searchFilter.StringFlag', 2);
+	})
+	let dispCodeAll = commands.registerCommand("searchFilter.codeAll", async () => {
+		commands.executeCommand('setContext', 'searchFilter.StringFlag', 0);
+	})
+
+	context.subscriptions.push(dispDoFilter);
+	context.subscriptions.push(dispDoNotFilter);
 	context.subscriptions.push(dispInComments);
 	context.subscriptions.push(dispNotInComments);
+	context.subscriptions.push(dispInString);
+	context.subscriptions.push(dispNotInString);
+	context.subscriptions.push(dispCodeAll);
+
+
 
 }
 
